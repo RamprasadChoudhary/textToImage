dd3681ffdf0f93a488a129ffd5d128d0cb35fb67
Ram Choudhary <ramchoudhary90860@gmail.com>
2025-01-07 16:45:30 +0530

    basic add

---
+++ client/src/components/Steps.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Steps = () => {
+  return (
+    <div>
+        <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>
+            How it works
+        </h1>
+
+        <p className='text-lg text-gray-600 mb-8'>Transform Works Into Stunning Images</p>
+        
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Steps
